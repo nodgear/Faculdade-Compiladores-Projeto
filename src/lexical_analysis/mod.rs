daf0b5065b891956ea789ebaff80dea24c9b74ca
Joao-Arthur <joao.lothamer@gmail.com>
2022-02-27 20:22:33 -0300

    setup unitary tests

--- src/lexical_analysis/mod.rs
+++ src/lexical_analysis/mod.rs
@@ -1,3 +1,13 @@
 pub fn execute() -> bool {
     return true;
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn expect_to_execute() {
+        assert_eq!(true, execute());
+    }
+}
