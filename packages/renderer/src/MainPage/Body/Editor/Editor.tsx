b789bfb85ea63d9f228d94c4f55e5ec1b775d772
Joao-Arthur <joao.lothamer@gmail.com>
2022-03-06 22:47:15 -0300

    add editor

--- packages/renderer/src/MainPage/Body/Editor/Editor.tsx
+++ packages/renderer/src/MainPage/Body/Editor/Editor.tsx
@@ -1,3 +1,17 @@
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/theme-monokai';
+
 export function Editor() {
-    return <></>;
+    return (
+        <div style={{ display: 'flex', height: '100%', width: '100%' }}>
+            <AceEditor
+                theme='monokai'
+                fontSize={20}
+                height='100%'
+                width='100%'
+                onChange={newValue => console.log(newValue)}
+                editorProps={{ $blockScrolling: true }}
+            />
+        </div>
+    );
 }
--- packages/renderer/src/MainPage/MainPage.tsx
+++ packages/renderer/src/MainPage/MainPage.tsx
@@ -1,11 +1,12 @@
 import { Body } from './Body';
 import { Header } from './Header';
+import { Container } from './MainPage.styles';
 
 export function MainPage() {
     return (
-        <>
+        <Container>
             <Header />
             <Body />
-        </>
+        </Container>
     );
 }
