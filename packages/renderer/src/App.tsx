32dcd2672d0e408e3083bb1ff3a34094ceafaeca
nodgear <matheus.silva3@ftc.edu.br>
2022-03-13 13:09:20 +0000

    feat: tailwindcss on vite+react

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { MainPage } from './MainPage';
 
+import '../global.css'
+
 export function App() {
     return (
         <StrictMode>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  jit: true,
+  content: ['./packages/renderer/src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
